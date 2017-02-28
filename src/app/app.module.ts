733955c857bbaa74690d3c7b37e03158d5c2b04a
Stephen Corry <stevemcorry@gmail.com>
2017-02-28 16:42:38 -0700

    routes try

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component'
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ProfileComponent,
-    LoginComponent,
-    HeaderComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot([
+const appRoutes: Routes = [
       {
         path: 'home',
         component: HomeComponent
@@ -40,9 +27,24 @@ import { HeaderComponent } from './header/header.component'
         path: 'header',
         component: HeaderComponent
       }
-    ])
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    LoginComponent,
+    HeaderComponent,
   ],
-  providers: [],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+  ],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
